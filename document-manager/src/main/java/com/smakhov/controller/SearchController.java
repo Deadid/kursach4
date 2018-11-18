45c3441273cde05f3a4e138309f2ec695e285167
Serhii Makhov <dead0896@gmail.com>
2018-11-18 14:25:49 +0200

    Updated spring. Index all fields.

--- document-manager/src/main/java/com/smakhov/controller/SearchController.java
+++ document-manager/src/main/java/com/smakhov/controller/SearchController.java
@@ -1,20 +1,11 @@
 package com.smakhov.controller;
 
 import com.smakhov.entity.ElasticsearchDocumentEntity;
-import org.elasticsearch.index.query.QueryStringQueryBuilder;
-import org.elasticsearch.search.aggregations.AggregationBuilders;
-import org.elasticsearch.search.aggregations.bucket.filters.FiltersAggregationBuilder;
-import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
-import org.elasticsearch.search.aggregations.support.ValueType;
-import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
-import org.elasticsearch.search.sort.ScoreSortBuilder;
-import org.elasticsearch.search.sort.SortBuilder;
 import org.elasticsearch.search.sort.SortBuilders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.core.aggregation.AggregatedPage;
-import org.springframework.data.elasticsearch.core.aggregation.impl.AggregatedPageImpl;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -35,7 +26,7 @@ public class SearchController {
   public AggregatedPage<ElasticsearchDocumentEntity> search(@RequestParam("content")String contentToSearch, @RequestParam(required = false, name = "page") Integer page) {
     NativeSearchQuery searchQuery = new NativeSearchQueryBuilder().withFields("*").withQuery(
             queryStringQuery(contentToSearch).defaultField("content")).withSort(SortBuilders.scoreSort()).withSort(SortBuilders.fieldSort("id.keyword"))
-            .withPageable(PageRequest.of(page != null? page : 0, 20))
+            .withPageable(PageRequest.of(page != null ? page : 0, 20))
             .build();
 
     AggregatedPage<ElasticsearchDocumentEntity> content = template.queryForPage(searchQuery, ElasticsearchDocumentEntity.class);
